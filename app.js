ba874435f67fb7de010750f5f054d6984b030310
sushberiwal <sushantberiwal@gmail.com>
2020-12-02 18:56:49 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const cors = require('cors')
+app.use(cors());
+const http = require("http").createServer(app);
+const io = require("socket.io")(http , {cors: {
+  origin: '*',
+}});
+// io pe => event listen
+
+
+io.on("connection", function (socket) {
+  console.log(`${socket.id} connected`);
+
+  socket.on("mousedown", function (data) {
+    socket.broadcast.emit("md", data);
+  });
+
+  socket.on("mousemove" , function(data){
+    socket.broadcast.emit("mm" , data);
+  })
+});
+
+app.get("/", function (req, res) {
+  res.send("<h1>Welcome to home page !!!</h1>");
+});
+
+let port = process.env.PORT || 3000;
+
+// app => api => server
+http.listen(port, () => {
+  console.log("listening on *:3000");
+});
